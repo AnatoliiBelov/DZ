4401f13ec2b14d11cc991f21eaa82b702e2cba25
AnatoliiBelov <anatoly.belov.ne@gmail.om>
2022-12-22 19:02:28 +0300

    Финиш ДЗ по классам, ч 1.

--- src/homework1/Main.java
+++ src/homework1/Main.java
@@ -6,11 +6,12 @@ public class Main {
         Author warAndPiece = new Author("Лев ", "Толстой");
         System.out.println(warAndPiece.firstName + warAndPiece.lastname);
         warAndPiece.setFirstName("ЛЕВ ");
-        System.out.println(warAndPiece.firstName+warAndPiece.lastname);
+        System.out.println(warAndPiece.firstName + warAndPiece.lastname);
 
         task1();
     }
-    public static void task1(){
+
+    public static void task1() {
         Book warAndPiece = new Book("War and Piece", 1757);
         System.out.println("warAndPiece.name = " + warAndPiece.getName());
         System.out.println("warAndPiece.dateOfPublication = " + warAndPiece.getDateOfPublication());
